8b721607f2cc37f64bd96dacb4aa95036f4f1fdb
Pradeep Nag <npav5057@gmail.com>
2020-11-30 10:24:11 +0530

    Buggy Code

--- structured_grid/main.py
+++ structured_grid/main.py
@@ -1,11 +1,12 @@
 from Solver import Solver
 from Meshing import struct_mesh
 import numpy as np
-from vtk.util.numpy_support import numpy_to_vtk
-import vtk
-# import time
+# from vtk.util.numpy_support import numpy_to_vtk
+# import vtk
+import time
+from mayavi import mlab
 
-
+    
 if __name__=='__main__':
     # tp=time.perf_counter()
     # p1=solver.solve2(meshg)
@@ -15,89 +16,28 @@ if __name__=='__main__':
     # print("fast tdma taken time:",time.perf_counter()-tp)
     # print(np.linalg.norm(p1-p2))
     dimensions=[1,1,1]
-    mesh_elem=[10,10,10]
+    mesh_elem=[20,20,20]
     # arr=np.arange(64).reshape((4,4,4))
     # print(arr[0,0,:])
 
-#     print("for shape",mesh_elem)
+    print("for shape",mesh_elem)
+    meshg=struct_mesh(dimensions,mesh_elem)
+    meshg.generate_mesh()
+    # solver=Solver(meshg)
+    # solver.set_conditions(meshg)
+    # tp=time.perf_counter()
+    # phi=solver.solve(meshg)
+    # print("fast tdma taken time:",time.perf_counter()-tp)
 
-
-#     meshg=struct_mesh(dimensions,mesh_elem)
-#     meshg.generate_mesh()
-#     solver=Solver(meshg)
-#     solver.set_conditions(meshg)
-#     tp=time.perf_counter()
-#     phi=solver.solve(meshg)
-#     print("fast tdma taken time:",time.perf_counter()-tp)
-
-#     np.savez('phi_10x10x10.npz', phi)
-
-    data=np.load('phi_10x10x10.npz')
-    phi=  data['arr_0']
-     phi=phi[:,:-1,:-1,:-1]
-    dims= phi.shape[1:]
-    print(dims)
+    data_st=np.load('phi_10x10x10.npz')
+    phi=  data_st['arr_0']
+    del data_st
     print(phi.shape)
-
-
-    colors = vtk.vtkNamedColors()
-
-    sgrid = vtk.vtkStructuredGrid()
-    sgrid.SetDimensions(dims)
-    points = vtk.vtkPoints()
-    data=vtk.vtkDoubleArray()
-    data.SetNumberOfComponents(1)
-    # cells=vtk.vtkCellData()
-
-    for i in range(dims[0]):
-        id=i*(dims[1]*dims[2])
-        for j in range(dims[1]):
-            id+=j*dims[2]
-            for k in range(dims[2]):
-                points.InsertNextPoint((i,j,k))
-                data.InsertNextValue(phi[4,i,j,k])
-    sgrid.SetPoints(points)
-    data.SetName("phi_val");
-    sgrid.GetPointData().SetScalars(data)
-    print(sgrid.GetScalers())
-    print(sgrid.GetNumberOfCells())
-    # print(sgrid.GetNumberOfPoints())
-#     print(sgrid.GetPointData().GetArray())
-
-
-    # sgridMapper = vtk.vtkPolyDataMapper()
-    # sgridMapper.SetInputConnection(hedgehog.GetOutputPort())
-    # sgridActor = vtk.vtkActor()
-    # sgridActor.SetMapper(sgridMapper)
-    # sgridActor.GetProperty().SetColor(colors.GetColor3d("Peacock"))
-
-    # # Create the usual rendering stuff
-    # renderer = vtk.vtkRenderer()
-    # renWin = vtk.vtkRenderWindow()
-    # renWin.AddRenderer(renderer)
-
-    # iren = vtk.vtkRenderWindowInteractor()
-    # iren.SetRenderWindow(renWin)
-
-    # renderer.AddActor(sgridActor)
-    # renderer.SetBackground(colors.GetColor3d("Beige"))
-    # renderer.ResetCamera()
-    # renderer.GetActiveCamera().Elevation(60.0)
-    # renderer.GetActiveCamera().Azimuth(30.0)
-    # renderer.GetActiveCamera().Dolly(1.25)
-    # renWin.SetSize(640, 480)
-
-    # # Interact with the data.
-    # renWin.Render()
-    # iren.Start()
-
-
-"""
 
     phi = np.swapaxes(phi,1,3)
     xx,yy,zz= np.meshgrid(meshg.xgrid,meshg.ygrid,meshg.zgrid,indexing='ij')
     print(xx.shape)
-
+    
     mlab.clf()
     pt = mlab.figure(size=(800,700))
     cphi=phi[0,1:-1,1:-1,1:-1]
@@ -113,7 +53,7 @@ if __name__=='__main__':
 #             mlab.outline()
             mlab.axes()
             mlab.savefig(filename ='ani_%02d.png'%t)
-            yield
+            yield          
     anim()
     mlab.view(distance=5)
     mlab.show()
@@ -121,20 +61,97 @@ if __name__=='__main__':
     import glob
     import os
     files=glob.glob("*.png")
-    list.sort(files
-              , key=lambda x: int(x.split('_')[1].split('.png')[0]))
+    list.sort(files, key=lambda x: int(x.split('_')[1].split('.png')[0]))
     print(len(files))
     with open('images.txt', 'w') as f:
         for item in files:
             f.write("%s\n" % item)
         f.close()
-    os.system('convert @images.txt {}.gif'.format("10x10x10_X_axix_YlOrBr")) # On windows convert is 'magick'
+    os.system('convert @images.txt {}.gif'.format("tttt_YlOrBr")) # On windows convert is 'magick'
     [os.remove(ff) for ff in files]
     os.remove('images.txt')
 
+"""
+#     np.savez('phi_10x10x10.npz', phi)
+   
+    data_st=np.load('phi_10x10x10.npz')
+    phi=  data_st['arr_0']
+    del data_st
+    phi=phi[:,:-1,:-1,:-1]
+    dims= phi.shape[1:]
+    print(phi.shape)
+    print(np.max(phi))
+    print(np.min(phi))
+
+
+    colors = vtk.vtkNamedColors()
+    grid = vtk.vtkStructuredPoints()
+    grid.SetDimensions(dims)
+    grid.SetOrigin(-0.5, -0.5, -0.5)
+    sp=1/200;
+    grid.SetSpacing(sp, sp, sp)
+    # print(sgrid.GetPoins())
+    # print(sgrid.GetNumberOfCells())
+    data=vtk.vtkDoubleArray()
+    data.SetNumberOfComponents(1)
+    data.SetNumberOfTuples((dims[0]-1)*(dims[1]-1) *(dims[2]-1))
+
+    # cells=vtk.vtkCellData()
+    for i in range(dims[0]):
+        ids=i*(dims[1]*dims[2])
+        for j in range(dims[1]):
+            ids+=j*dims[2]
+            for k in range(dims[2]):
+                data.InsertTuple1(ids+k,phi[4,i,j,k])
+                # points.InsertNextPoint(i,j,k)
+                # data.InsertNextValue(phi[4,i,j,k])
+                
+    # grid.SetPoints(points)
+    data.SetName("phi_val");
+    grid.GetPointData().SetScalars(data)
+    colorLookupTable = vtk.vtkLookupTable()
+    colorLookupTable.SetTableRange(0, np.max(phi))
+    colorLookupTable.Build()    
+    contour = vtk.vtkContourFilter()
+    contour.SetInputData(grid)
+    contour.SetValue(0, 5)
+
+    renderer = vtk.vtkRenderer()
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(renderer)
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(renWin)
+
+    volMapper = vtk.vtkPolyDataMapper()
+    volMapper.SetInputConnection(contour.GetOutputPort())
+    volMapper.ScalarVisibilityOff()
+    volActor = vtk.vtkActor()
+    volActor.SetMapper(volMapper)
+    volActor.GetProperty().EdgeVisibilityOn()
+    volActor.GetProperty().SetColor(colors.GetColor3d("Red"))
+    renderer.AddActor(volActor)
+    renderer.SetBackground(colors.GetColor3d("SlateGray"))
+    renWin.SetSize(512, 512)
+    # Interact with the data.
+    # writer = vtk.vtkStructuredPointsWriter()
+    # writer.SetInputData(grid)
+    # writer.SetFileName('data.vtk')
+    # writer.Write()
+
+    renWin.Render()
+    iren.Start()
+
+    # print(sgrid.GetPointData())
+    # print(sgrid.GetNumberOfCells())
+
+    # print(sgrid.GetNumberOfPoints())  
+"""
+
+    
+    
 #     ani = ArtistAnimation(files, interval=50, blit=True,repeat_delay=1000)
 #     ani.save('dynamic_images.gif')
-
+    
 #     fig = plt.figure()
 #     ax = plt.axes(projection='3d')
 #     ax.contourf(phi[2,1:-1,1:-1,1:-1], 20 , cmap = "RdGy")
@@ -143,4 +160,3 @@ if __name__=='__main__':
 #     ax.set_ylabel('y')
 #     ax.set_zlabel('z')
 #     plt.show()
-"""
