2130543ffbe370693eadbfd45c572b459aa733de
strawhat04 <nabeelk04@gmail.com>
2021-01-09 22:09:36 +0530

    Included Explicit Solver

--- structured_grid/Solver.py
+++ structured_grid/Solver.py
@@ -4,7 +4,7 @@ from StructuralDiscre import LinFlux
 from Meshing import struct_mesh
 from parameters import runtime, dt
 
-print("runttime",runtime)
+
 #Class to solver
 class solverCLS(struct_mesh):
     runtime=runtime
@@ -31,8 +31,6 @@ class solverCLS(struct_mesh):
     def set_conditions(self,mesh):
         iphi=-1
         self.phi=iphi*np.ones((int(self.runtime/self.dt)+1,mesh.nz+2,mesh.ny+2,mesh.nx+2)) #include 2 ghost nodes
-        print(self.phi.shape)
-
 
         #Put initital condition here
         #self.phi[0,int(mesh.nz/2-mesh.nz/5):int(mesh.nz/2+mesh.nz/5),int(mesh.ny/2-mesh.ny/5):int(mesh.ny/2+mesh.ny/5),int(mesh.nx/2-mesh.nx/5):int(mesh.nx/2+mesh.nx/5)]=10
@@ -50,6 +48,7 @@ class solverCLS(struct_mesh):
         self.bc,x0,y0,z0,xn,yn,zn=['d',5,4,1,6,2,9]
         #Computing flux coefficient for dirichlet condition
         if self.bc=='D' or self.bc=='d':
+            
             self.phi[:,:,1,:]=y0
             self.phi[:,1,:,:]=z0
             self.phi[:,:,:,1]=x0
@@ -70,12 +69,12 @@ class solverCLS(struct_mesh):
             self.ab[0,:,:]=self.af[nx,:,:]=0
             self.b[0,:,:]+=float(z0)*Sz
             self.b[nx,:,:]+=float(zn)*Sz
-        print(self.phi[0,1:3,1:-1,1:-1])
+        #print(self.phi[0,1:3,1:-1,1:-1])
 
 
-    #this function solve linear equation array by array using line by line method
-    #which is a combination of Gauss-Seidel and TDMA, we are implementing TDMA in x direction and Gauss-seidel in rest
-    #Refer this solver function rather the vector solver for ease of understanding
+    ''' this function solve linear equation array by array using line by line method
+    which is a combination of Gauss-Seidel and TDMA, we are implementing TDMA in x direction and Gauss-seidel in rest
+    Refer this solver function rather the vector solver for ease of understanding '''
     def solve(self,mesh):
         #since this is a iterative solver 
         #rdue solve the residual value of phi at each iteration means 
@@ -141,8 +140,8 @@ class solverCLS(struct_mesh):
         d=np.zeros((mesh.nz,mesh.ny,mesh.nx),)
         R=np.zeros((mesh.nz,mesh.ny,mesh.nx+2),)
         Q=np.zeros((mesh.nz,mesh.ny,mesh.nx+2),)
-        print("vectorized  TDMA solver")
-        print(self.phi.shape)
+        # print("vectorized  TDMA solver")
+        # print(self.phi.shape)
         
         #these Bc need to implement for TDMA specifically
         Q[:,:,0]=R[:,:,0]=0
@@ -185,6 +184,7 @@ class solverCLS(struct_mesh):
                     # self.phi[t,k+1,j+1,i+1]=R[i+1]*self.phi[t,k+1,j+1,i+2]+Q[i+1]
                     self.phi[t,1:self.dim[0]+1,1:self.dim[1]+1,i+1]=np.multiply(R[:,:,i+1],self.phi[t,1:self.dim[0]+1,1:self.dim[1]+1,i+2]) +Q[:,:,i+1]
                 
+                #Forced Boundary Condition in other direction 
                 if self.bc=='D' or self.bc=='d':
                     self.phi[t,:,1,:]=self.phi[t-1,:,1,:]
                     self.phi[t,:,mesh.ny,:]=self.phi[t-1,:,mesh.ny,:]
@@ -194,8 +194,167 @@ class solverCLS(struct_mesh):
                 rdue=self.phi[t,2:-2,2:-2,2:-2]-np.divide((np.multiply(self.aw[1:-1,1:-1,1:-1],self.phi[t,2:-2,2:-2,1:-3])+np.multiply(self.ae[1:-1,1:-1,1:-1],self.phi[t,2:-2,2:-2,3:-1])+np.multiply(self.au[1:-1,1:-1,1:-1],self.phi[t,2:-2,3:-1,2:-2])+np.multiply(self.ad[1:-1,1:-1,1:-1], self.phi[t,2:-2,1:-3,2:-2])+np.multiply(self.af[1:-1,1:-1,1:-1],self.phi[t,3:-1,2:-2,2:-2])+np.multiply(self.ab[1:-1,1:-1,1:-1], self.phi[t,1:-3,2:-2,2:-2])+self.b[1:-1,1:-1,1:-1]+ np.multiply(self.ap0[1:-1,1:-1,1:-1],self.phi[t-1,2:-2,2:-2,2:-2])),self.ap[1:-1,1:-1,1:-1])
 
                 err=np.linalg.norm(rdue)
-                if err>1e-8:
+                if err>1e-4:
                     # print("At itr=",itr," Error:",err)
+                    itr+=1
+                    if(np.absolute(err-error_init)<1e-6):
+                        break
+                    error_init=err
+                    if itr>1000:
+                        break
+                else:
+                    #print("done for time t:",t*self.dt," sec, Total taken itr:",itr)
+                    break
+            print("for t=", t, "At itr=",itr," Error:",err)
+        return self.phi
+
+    def ADIsolver(self,mesh):
+        self.ap+=2*self.ap0
+        self.ap0=3*self.ap0
+        #print(self.ap0)
+        # print("*******************************")
+        # print(3*self.ap0)
+        rdue=np.ones((self.dim[0]-2,self.dim[1]-2,self.dim[2]-2),)
+        d=np.zeros((mesh.nz,mesh.ny,mesh.nx),)
+        R=np.zeros((mesh.nz+2,mesh.ny+2,mesh.nx+2),)
+        Q=np.zeros((mesh.nz+2,mesh.ny+2,mesh.nx+2),)
+        # print("vectorized  TDMA solver")
+        # print(self.phi.shape)
+        phix=np.zeros((mesh.nz+2,mesh.ny+2,mesh.nx+2))
+        phiy=np.zeros((mesh.nz+2,mesh.ny+2,mesh.nx+2))
+        #these Bc need to implement for TDMA specifically
+        
+        
+        #This Boundary condtion need to be implemented due to nature of TDMA Solver
+        
+
+        for t in range(1,self.phi.shape[0]):
+            # print("calculating for t=",t*dt," sec")
+            itr=1
+            rdue.fill(1)
+            error_init=0
+            self.phi[t,:,:,:]=self.phi[t-1,:,:,:]
+            
+            while itr<4:
+                
+                if itr%3==0:
+                    #print("When TDMA in X direction :")
+
+                #TDMA in X direction
+                    Q[:,:,1]=self.phi[0,0,0,1]
+                    Q[:,:,mesh.nx]=self.phi[0,0,0,mesh.nx]
+                    for i in range(1, self.dim[2]):
+                        #                               self.b[k,j,i]+self.ap0[k,j,i]*self.phi[t-1,k+1,j+1,i+1]
+                        d[:,:,i]=self.b[:,:,i]+np.multiply(self.ap0[:,:,i],self.phi[t-1,1:self.dim[0]+1,1:self.dim[1]+1,i+1])
+                        #                               self.au[k,j,i]*self.phi[t,k+1,j+1+1,i+1]
+                        d[:,:,i]+=np.multiply(self.au[:,:,i],self.phi[t,1:self.dim[0]+1,2:self.dim[1]+2,i+1])
+
+                        #                               self.ad[k,j,i]*self.phi[t,k+1,j,i+1]
+                        d[:,:,i]+=np.multiply(self.ad[:,:,i],self.phi[t,1:self.dim[0]+1,:self.dim[1],i+1])
+                                                        # self.af[k,j,i]*self.phi[t,k+1+1,j+1,i+1]
+                        d[:,:,i]+=np.multiply(self.af[:,:,i],self.phi[t,2:self.dim[0]+2,1:self.dim[1]+1,i+1])
+
+                                                 # self.ab[k,j,i]*self.phi[t,k+1-1,j+1,i+1]
+                        d[:,:,i]+=np.multiply(self.ab[:,:,i],self.phi[t,:self.dim[0],1:self.dim[1]+1,i+1])
+                        R[1:-1,1:-1,i+1]=self.ae[:,:,i]/(self.ap[:,:,i]-self.aw[:,:,i]*R[1:-1,1:-1,i])
+                        Q[1:-1,1:-1,i+1]=(d[:,:,i]+self.aw[:,:,i]*Q[1:-1,1:-1,i])/(self.ap[:,:,i]-self.aw[:,:,i]*R[1:-1,1:-1,i])
+
+                    #Forced TDMA Boundary Condition
+                    Q[:,:,1]=self.phi[0,0,0,1]
+                    R[:,:,1]=0
+                    Q[:,:,mesh.nx]=self.phi[0,0,0,mesh.nx]
+                    R[:,:,mesh.nx]=0
+
+                    for i in range(self.dim[2]-1,0,-1):
+                        # self.phi[t,k+1,j+1,i+1]=R[i+1]*self.phi[t,k+1,j+1,i+2]+Q[i+1]
+                        self.phi[t,1:self.dim[0]+1,1:self.dim[1]+1,i+1]=np.multiply(R[1:-1,1:-1,i+1],self.phi[t,1:self.dim[0]+1,1:self.dim[1]+1,i+2]) +Q[1:-1,1:-1,i+1]
+                   
+                if itr%3==2:
+                    #print("When TDMA in Y direction :")
+                    #TDMA in y direction
+                    Q[:,1,:]=self.phi[0,0,1,0]
+                    Q[:,mesh.ny,:]=self.phi[0,0,mesh.ny,0]
+
+                    #print( Q[:,1,:], Q[:,mesh.ny,:])
+                    for j in range(1, self.dim[1]):
+                        #                               self.b[k,j,i]+self.ap0[k,j,i]*self.phi[t-1,k+1,j+1,i+1]
+                        d[:,j,:]=self.b[:,j,:]+np.multiply(self.ap0[:,j,:],self.phi[t-1,1:self.dim[0]+1,j+1,1:self.dim[2]+1])
+                        #                               self.au[k,j,i]*self.phi[t,k+1,j+1+1,i+1]
+                        d[:,j,:]+=np.multiply(self.ae[:,j,:],self.phi[t,1:self.dim[0]+1,j+1,2:self.dim[2]+2])
+
+                        #                               self.ad[k,j,i]*self.phi[t,k+1,j,i+1]
+                        d[:,j,:]+=np.multiply(self.aw[:,j,:],self.phi[t,1:self.dim[0]+1,j+1,:self.dim[2]])
+                                                        # self.af[k,j,i]*self.phi[t,k+1+1,j+1,i+1]
+                        d[:,j,:]+=np.multiply(self.af[:,j,:],self.phi[t,2:self.dim[0]+2,j+1,1:self.dim[2]+1])
+
+                                                 # self.ab[k,j,i]*self.phi[t,k+1-1,j+1,i+1]
+                        d[:,j,:]+=np.multiply(self.ab[:,j,:],self.phi[t,:self.dim[0],j+1,1:self.dim[2]+1])
+                        #print("in print",R[:,j+1,:].shape,self.au[:,j,:].shape,self.ap[:,j,:].shape,self.ad[:,j,:].shape )
+                        R[1:-1,j+1,1:-1]=self.au[:,j,:]/(self.ap[:,j,:]-self.ad[:,j,:]*R[1:-1,j,1:-1])
+                        
+                        Q[1:-1,j+1,1:-1]=(d[:,j,:]+self.ad[:,j,:]*Q[1:-1,j,1:-1])/(self.ap[:,j,:]-self.ad[:,j,:]*R[1:-1,j,1:-1])
+
+                    #Forced TDMA Boundary Condition
+                    Q[:,1,:]=self.phi[0,0,1,0]
+                    R[:,1,:]=0
+                    Q[:,mesh.ny,:]=self.phi[0,0,mesh.ny,0]
+                    R[:,mesh.ny,:]=0
+
+                    
+                    for j in range(self.dim[1]-1,0,-1):
+                        # self.phi[t,k+1,j+1,i+1]=R[i+1]*self.phi[t,k+1,j+1,i+2]+Q[i+1]
+                        self.phi[t,1:self.dim[0]+1,j+1,1:self.dim[2]+1]=np.multiply(R[1:-1,j+1,1:-1],self.phi[t,1:self.dim[0]+1,j+2,1:self.dim[2]+1]) +Q[1:-1,j+1,1:-1]
+                
+                if itr%3==1:
+                    #print("When TDMA in Z direction :")
+
+                #TDMA in X direction
+                    Q[1,:,:]=self.phi[0,1,0,0]
+                    Q[mesh.nz,:,:]=self.phi[0,mesh.nz,0,0]
+                    for k in range(1, self.dim[0]):
+                        #                               self.b[k,j,i]+self.ap0[k,j,i]*self.phi[t-1,k+1,j+1,i+1]
+                        d[k,:,:]=self.b[k,:,:]+np.multiply(self.ap0[k,:,:],self.phi[t-1,k+1,1:self.dim[1]+1,1:self.dim[2]+1])
+                        #                               self.au[k,j,i]*self.phi[t,k+1,j+1+1,i+1]
+                        d[k,:,:]+=np.multiply(self.au[k,:,:],self.phi[t,k+1,2:self.dim[1]+2,1:self.dim[2]+1])
+
+                        #                               self.ad[k,j,i]*self.phi[t,k+1,j,i+1]
+                        d[k,:,:]+=np.multiply(self.ad[k,:,:],self.phi[t,k+1,:self.dim[1],1:self.dim[2]+1])
+                                                        # self.af[k,j,i]*self.phi[t,k+1+1,j+1,i+1]
+                        d[k,:,:]+=np.multiply(self.ae[k,:,:],self.phi[t,k+1,1:self.dim[1]+1,2:self.dim[2]+2])
+
+                        #                               self.ad[k,j,i]*self.phi[t,k+1,j,i+1]
+                        d[k,:,:]+=np.multiply(self.aw[k,:,:],self.phi[t,k+1,1:self.dim[1]+1,:self.dim[2]])
+                        
+                        R[k+1,1:-1,1:-1]=self.ae[k,:,:]/(self.ap[k,:,:]-self.aw[k,:,:]*R[k,1:-1,1:-1])
+                        Q[k+1,1:-1,1:-1]=(d[k,:,:]+self.aw[k,:,:]*Q[k,1:-1,1:-1])/(self.ap[k,:,:]-self.aw[k,:,:]*R[k,1:-1,1:-1])
+
+                    #Forced TDMA Boundary Condition
+                    Q[1,:,:]=self.phi[0,1,0,0]
+                    R[1,:,:]=0
+                    Q[mesh.nz,:,:]=self.phi[0,mesh.nz,0,0]
+                    R[mesh.nz,:,:]=0
+
+                    for k in range(self.dim[0]-1,0,-1):
+                        # self.phi[t,k+1,j+1,i+1]=R[i+1]*self.phi[t,k+1,j+1,i+2]+Q[i+1]
+                        self.phi[t,k+1,1:self.dim[1]+1,1:self.dim[2]+1]=np.multiply(R[k+1,1:-1,1:-1],self.phi[t,k+2,1:self.dim[1]+1,1:self.dim[2]+1]) +Q[k+1,1:-1,1:-1]
+                   
+
+                #Forced Boundary Condition in other direction 
+                if self.bc=='D' or self.bc=='d':
+                    self.phi[t,:,:,1]=self.phi[t-1,:,:,1]
+                    self.phi[t,:,:,mesh.nx]=self.phi[t-1,:,:,mesh.nx]
+                    self.phi[t,:,1,:]=self.phi[t-1,:,1,:]
+                    self.phi[t,:,mesh.ny,:]=self.phi[t-1,:,mesh.ny,:]
+                    self.phi[t,1,:,:]=self.phi[t-1,1,:,:]
+                    self.phi[t,mesh.nz,:,:]=self.phi[t-1,mesh.nz,:,:]
+
+                    #self.phi=self.phi/2
+                rdue=self.phi[t,2:-2,2:-2,2:-2]-np.divide((np.multiply(self.aw[1:-1,1:-1,1:-1],self.phi[t,2:-2,2:-2,1:-3])+np.multiply(self.ae[1:-1,1:-1,1:-1],self.phi[t,2:-2,2:-2,3:-1])+np.multiply(self.au[1:-1,1:-1,1:-1],self.phi[t,2:-2,3:-1,2:-2])+np.multiply(self.ad[1:-1,1:-1,1:-1], self.phi[t,2:-2,1:-3,2:-2])+np.multiply(self.af[1:-1,1:-1,1:-1],self.phi[t,3:-1,2:-2,2:-2])+np.multiply(self.ab[1:-1,1:-1,1:-1], self.phi[t,1:-3,2:-2,2:-2])+self.b[1:-1,1:-1,1:-1]+ np.multiply(self.ap0[1:-1,1:-1,1:-1],self.phi[t-1,2:-2,2:-2,2:-2])),self.ap[1:-1,1:-1,1:-1])
+
+                err=np.linalg.norm(rdue)
+                #print("in while", itr, err)
+                if err>1e-12:
+                    print("At itr=",itr," Error:",err)
                     itr+=1
                     if(np.absolute(err-error_init)<1e-12):
                         break
@@ -203,8 +362,10 @@ class solverCLS(struct_mesh):
                     if itr>1000:
                         break
                 else:
-                    print("done for time t:",t*self.dt," sec, Total taken itr:",itr)
+                    #print("done for time t:",t*self.dt," sec, Total taken itr:",itr)
                     break
+
+            print("for t=", t, "At itr=",itr," Error:",err)
         return self.phi
 
 #     def solve1(self):
--- structured_grid/main.py
+++ structured_grid/main.py
@@ -13,16 +13,16 @@ import matplotlib.animation as animation
 
 
 dimensions=[2,2,2]
-mesh_grid=[20,18,15]
+mesh_grid=[50,50,50]
 # arr=np.arange(64).reshape((4,4,4))
 # print(arr[0,0,:])
 xGRID_nos=mesh_grid[0]
-print("for shape",mesh_grid)
+#print("for shape",mesh_grid)
 
 meshg=struct_mesh(dimensions,mesh_grid)
 meshg.generate_mesh()
 
-#dill.dump(meshg, file = open("mesh20-18-15.pickle", "wb"))
+#dill.dump(meshg, file = open("mesh16-20-15.pickle", "wb"))
 
 solver=solverCLS(meshg)
 solver.set_conditions(meshg)
@@ -31,36 +31,46 @@ tp=time.perf_counter()
 # print("slow tdma taken time:",time.perf_counter()-tp)
 # tp=time.perf_counter()
 phi=solver.solveByVector(meshg)
-np.save('phi',phi)
+# print("old TDMA: ", time.perf_counter()-tp)
+# tp=time.perf_counter()
+# phi=solver.ADIsolver(meshg)
+print("new TDMA: ", time.perf_counter()-tp)
 
-print("fast tdma taken time:",time.perf_counter()-tp)
+np.save('phipure',phi)
+
+#print("fast tdma taken time:",time.perf_counter()-tp)
 #print(np.linalg.norm(p1-phi))
-#print(phi[-1,int(zGRID_nos/2), :,:])
 
-y,z= np.meshgrid(meshg.cvygrid,meshg.cvzgrid)
+
+z,y=np.meshgrid(meshg.cvzgrid,meshg.cvygrid)
 fig,ax=plt.subplots(figsize=(10,5))
-
+print("hererere" , z.shape, y.shape, phi[0,1:-1,1:-1,int(xGRID_nos/2)].shape)
 #PLOT COLOR MESH
 cmin=np.min(phi[:,1:-1,1:-1,int(xGRID_nos/2)])
-print(cmin)     
+#print(cmin)     
 cmax=np.max(phi[:,1:-1,1:-1,int(xGRID_nos/2)])
-print(cmax)
-cs=ax.pcolormesh(y,z,phi[0,1:-1,1:-1,int(xGRID_nos/2)], cmap=cm.RdPu, vmin=cmin,vmax=cmax)
+#print(cmax)
+cs=ax.pcolormesh(z,y,np.transpose(phi[0,1:-1,1:-1,int(xGRID_nos/2)]), cmap=cm.RdPu, vmin=cmin,vmax=cmax)
 
 cbar=fig.colorbar(cs)
+plt.xticks(fontsize=16)
+plt.yticks(fontsize=16)
+ax.set_xlabel("Y axis\nDistance(m)\n",fontsize=16)
+ax.set_ylabel("Z axis\nDistance(m)\n",fontsize=16)
+ax.set_title("YZ plane view at centroid of domain for Pure Advection\n with gamma=0, uy=1, ux=uz=0",fontsize=16)
+ax.text(0,-1/7,'at time= 0 sec\n',fontsize=16)
 
-ax.set_xlabel("Distnace (m)")
-ax.set_ylabel("Distance (m)")
-ax.set_title("2D Advection-Diffusion FVM Solver\n with Dirichlet boundary Condition at x=0 and y=0")
-
+#plt.show()
 #Func TO ANIMATE THE PLOT
 def update(t):
     for txt in ax.texts:
         txt.set_visible(False)
-    ax.pcolormesh(y,z,phi[t,1:-1,1:-1,int(xGRID_nos/2)], cmap=cm.RdPu, vmin=cmin,vmax=cmax)
-    ax.text(0,-1/8,'at time= %f sec\nux=2, uy=2 & gamma=3'%(t*dt), size=10)
+    ax.pcolormesh(z,y,np.transpose(phi[t,1:-1,1:-1,int(xGRID_nos/2)]), cmap=cm.RdPu, vmin=cmin,vmax=cmax)
+    ax.text(0,-1/6,'at time= %f sec\n'%(t*dt), size=10,fontsize=16)
 
 sim= animation.FuncAnimation(fig,update, frames=range(0,len(phi[:,0,0,0])), interval=500, repeat=False)
+#sim.save('uycouse.gif', writer='imagemagick')
+
 plt.show()
 
 
