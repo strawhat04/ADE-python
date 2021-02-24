a3397f7b714e049d4b9ea2f009043d3664fc7592
strawhat04 <nabeelk04@gmail.com>
2021-02-24 15:25:13 +0530

    included output directory

--- structured_grid/BoundaryCondition.py
+++ structured_grid/BoundaryCondition.py
@@ -1,75 +1,87 @@
 import numpy as np
-
 '''
 d>dirichlet
-n>newman
+f>neumann flux
 a>advective
 
 ''' 
 
-def Bcond(phi, b, Sx, Sy, Sz,t, nx, ny, nz):
+def Bcond(phi, Sx, Sy, Sz,t, nx, ny, nz, ap, aw, ae, ad, au, ab, af, ap0, b, Q,R):
+    # R=np.zeros((nz,ny,nx+2),)
+    # Q=np.zeros((nz,ny,nx+2),)
+    
+    #these Bc need to implement for TDMA specifically
+    Q[:,:,0]=R[:,:,0]=0
+
     bc=[]
 
-    bc.append({'type':'d', 'val':7})	#x0 face
-    bc.append({'type':'a', 'val':7})	#y0 face
-    bc.append({'type':'d', 'val':7})	#z0 face
-    bc.append({'type':'a', 'val':7})	#xn face
-    bc.append({'type':'a', 'val':7})	#yn face
-    bc.append({'type':'a', 'val':7})	#zn face
+    bc.append({'type':'d', 'val':20})   #x0 face
+    bc.append({'type':'d', 'val':15})   #y0 face
+    bc.append({'type':'d', 'val':10})   #z0 face
+    bc.append({'type':'d', 'val':5})    #xn face
+    bc.append({'type':'d', 'val':20})   #yn face
+    bc.append({'type':'d', 'val':0})    #zn face
     #Computing flux coefficient for dirichlet condition
 
     #for x0 face boundary
     if bc[0]['type']=='d':
-    	phi[t,:,:,1]=bc[0]['val']
+        phi[t,:,:,1]=bc[0]['val']
+        Q[:,:,1]=phi[t,1:-1,1:-1,1]
+        R[:,:,1]=0
     if bc[0]['type']=='a':
-    	pass
+        pass
     if bc[0]['type']=='n':
-    	aw[:,:,0]=0
-    	b[:,:,0]+=bc[0]['val']*Sx
+        aw[:,:,0]=0
+        b[:,:,0]=bc[0]['val']*Sx
+
 
     #for y0 face boundary
     if bc[1]['type']=='d':
-    	phi[t,:,1,:]=bc[1]['val']
+        phi[t,:,1,:]=bc[1]['val']
     if bc[1]['type']=='a':
-    	pass
-    if bc[1]['type']=='n':
-    	ad[:,1,:]=0
-    	b[:,1,:]+=bc[1]['val']*Sy
+        pass
+    if bc[1]['type']=='f':
+        ad[:,1,:]=0
+        b[:,0,:]=bc[1]['val']*Sy
 
     #for z0 face bcoundary
     if bc[2]['type']=='d':
-    	phi[t,1,:,:]=bc[2]['val']
+        phi[t,1,:,:]=bc[2]['val']
     if bc[2]['type']=='a':
-    	pass
-    if bc[2]['type']=='n':
-    	abc[2,:,:]=0
-    	b[2,:,:]+=bc[2]['val']*Sz
+        pass
+    if bc[2]['type']=='f':
+        ab[2,:,:]=0
+        b[0,:,:]=bc[2]['val']*Sz
 
     #for xn face boundary
     if bc[3]['type']=='d':
-    	phi[t,:,:,nx]=bc[3]['val']
+        phi[t,:,:,nx]=bc[3]['val']
+        Q[:,:,nx]=phi[t,1:-1,1:-1,nx]
+        R[:,:,nx]=0
+
     if bc[3]['type']=='a':
-    	pass
-    if bc[3]['type']=='n':
-    	aw[:,:,nx]=0
-    	b[:,:,nx]+=bc[3]['val']*Sx
+        pass
+    if bc[3]['type']=='f':
+        ae[:,:,nx-1]=0
+        b[:,:,nx-1]=bc[3]['val']*Sx
+        # Q[:,:,nx]=b[:,:,nx-1]/ap[:,:,nx-1]
 
     #for yn face boundary
     if bc[4]['type']=='d':
-    	phi[t,:,ny,:]=bc[4]['val']
+        phi[t,:,ny,:]=bc[4]['val']
     if bc[4]['type']=='a':
-    	pass
-    if bc[4]['type']=='n':
-    	aw[:,ny,:]=0
-    	b[:,ny,:]+=bc[4]['val']*Sy
+        pass
+    if bc[4]['type']=='f':
+        au[:,ny-1,:]=0
+        b[:,ny-1,:]=bc[4]['val']*Sy
 
     #for zn face boundary
     if bc[5]['type']=='d':
-    	phi[t,nz,:,:]=bc[5]['val']
+        phi[t,nz,:,:]=bc[5]['val']
     if bc[5]['type']=='a':
-    	pass
-    if bc[5]['type']=='n':
-    	aw[nz,:,:]=0
-    	b[nz,:,:]+=bc[5]['val']*Sz
+        pass
+    if bc[5]['type']=='f':
+        af[nz-1,:,:]=0
+        b[nz-1,:,:]=bc[5]['val']*Sz
 
-    return phi, b	
+    return phi, b, Q, R
